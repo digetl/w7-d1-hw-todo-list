2d394c06b27b92fe27901c883885cf0d3e5a2ecc
Dave Campbell <dave@digetl.co.uk>
2020-05-11 20:41:11 +0100

    setup app.js - added data and functions

--- src/app.js
+++ src/app.js
@@ -3,8 +3,26 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
-   
-      
+    data: {
+      todos: [
+        {name: "Load the dishwasher", isPriority: false},
+        {name: "Read Ethan a nighttime story", isPriority: true},
+        {name: "Make a cup of tea", isPriority: false}
+        ],
+        newTodo:""
+    },
+    methods: {
+        saveNewTodo: function(){
+            this.todo.push({
+                name: this.newTodo,
+                isPriority: false
+            });
+            this.newTodo = "";
+        },
+        makePriority: function(index){
+            this.todos[index].isPriority = true;
+        }
+    }
     
   });
 });
